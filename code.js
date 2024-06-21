0c4923821ba0cbe6607133b968ac7e335cdbe85c
wbds9650 <wd9650@gmail.com>
2024-06-22 08:11:40 +1200

    theme change

---
+++ code.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+    $('.shadow2').mouseenter(function(){
+        $(this).css({'background-color':'rgba(164, 217, 85, 0.623)'});
+    });
+
+    $('.shadow2').mouseleave(function(){
+        $(this).css({'background-color':'rgb(7, 21, 11)'});
+    });
+
+    $('.shadow1').mouseenter(function(){
+        $(this).css({'background-color':'rgba(164, 217, 85, 0.623)'});
+    });
+
+    $('.shadow1').mouseleave(function(){
+        $(this).css({'background-color':'transparent'});
+    });
+
+    $('.crd').mouseenter(function(){
+        $(this).css({'background-color':'rgba(11, 112, 11, 0.858)'});
+    });
+
+    $('.crd').mouseleave(function(){
+        $(this).css({'background-color':'rgb(192, 239, 123)'});
+    });
+
+    $('.crd2').mouseenter(function(){
+        $(this).css({'background-color':'rgba(11, 112, 11, 0.858)'});
+    });
+
+    $('.crd2').mouseleave(function(){
+        $(this).css({'background-color':'rgb(192, 239, 123)'});
+    });
+});
