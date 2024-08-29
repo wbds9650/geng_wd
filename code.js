049110928650465e237a03942e93681e2193bf46
wbds9650 <wd9650@gmail.com>
2024-08-30 05:03:53 +1200

    animation

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     $('.shadow2').mouseenter(function(){
   
-        $(this).css({'border':"7px solid green"})
+        $(this).css({'border':"4px solid green"})
     });
 
     $('.shadow2').mouseleave(function(){
@@ -11,7 +11,7 @@ $(document).ready(function(){
     });
 
     $('.shadow1').mouseenter(function(){
-        $(this).css({'border':"7px solid green"})
+        $(this).css({'border':"4px solid green"})
     });
 
     $('.shadow1').mouseleave(function(){
@@ -19,7 +19,7 @@ $(document).ready(function(){
     });
 
     $('.crd2').mouseenter(function(){
-        $(this).css({'border':"7px solid green"})
+        $(this).css({'border':"4px solid green"})
     });
 
     $('.crd2').mouseleave(function(){
@@ -28,13 +28,13 @@ $(document).ready(function(){
 
     $('.ig1').mouseenter(function(){
         $(this).css({'width': '110px'});
-        $(this).css({'border-radius': '20px'});
+        $(this).css({'border-radius': '40px'});
         $(this).css({'box-shadow': 'rgba(14, 153, 9, 0.4) 5px 5px, rgba(14, 153, 9, 0.4) 10px 10px, rgba(14, 153, 9, 0.4) 15px 15px, rgba(14, 153, 9, 0.4) 20px 20px, rgba(14, 153, 9, 0.4) 25px 25px'})
     });
 
     $('.ig1').mouseleave(function(){
         $(this).css({'width': '80px'});
-        $(this).css({'border-radius': '30px'});
+        $(this).css({'border-radius': '20px'});
         $(this).css({'box-shadow': 'none'})
     });
 
