8637d1f61c53f2d6697530392546c7716733613c
wbds9650 <wd9650@gmail.com>
2024-09-02 03:09:47 +1200

    new project

---
+++ Ecommerce/ecom.js
@@ -0,0 +1,12 @@
+
+function shownavbar() {
+    var sdn = document.querySelector(".sdn");
+    sdn.style.left = "0px";
+
+}
+
+function hidenavbar() {
+    var sdn = document.querySelector(".sdn");
+    sdn.style.left="-660px"
+}
+
